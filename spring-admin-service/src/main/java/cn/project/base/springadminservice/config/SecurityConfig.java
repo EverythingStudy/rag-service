0f393a223a4e5b70175080e27a26d15ca95eee5a
chenlinya <759499199@qq.com>
2025-03-25 09:54:33 +0800

    spring admin server Security

--- spring-admin-service/src/main/java/cn/project/base/springadminservice/config/SecurityConfig.java
+++ spring-admin-service/src/main/java/cn/project/base/springadminservice/config/SecurityConfig.java
@@ -17,35 +17,16 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/assets/**", "/manage/**", "/actuator/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout(logout -> logout.logoutUrl("/logout").permitAll())
+                .csrf(csrf -> csrf.disable());
         return http.build();
     }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/login", "/assets/**", "/manage/**", "/actuator/**").permitAll()  // 允许访问的URL路径
-//                .anyRequest().authenticated()  // 其他请求需要认证
-//                .and()
-//                .formLogin()  // 表单登录
-//                .loginPage("/login")  // 登录页面
-//                .permitAll()
-//                .and()
-//                .logout()  // 注销
-//                .logoutUrl("/logout")  // 注销URL
-//                .permitAll()
-//                .and()
-//                .csrf().disable();  // 禁用CSRF保护（仅用于示例，生产环境中应启用）
-//    }
 
     /**
      * 在上面的示例中，{noop}表示明文密码。在生产环境中，应使用更安全的密码加密方式。
